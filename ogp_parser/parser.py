9668f40bec4b47c264c0b873f4902af6af16f787
ukyoda <black12172001@hotmail.co.jp>
2018-12-24 00:07:48 +0900

    パーサー完成

--- ogp_parser/parser.py
+++ ogp_parser/parser.py
@@ -1,7 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-def parser(url):
+def __parse_ogp(metas):
+    ogps = list(filter(lambda x: x.has_attr('property'), metas))
+    results = {}
+    for ogp in ogps:
+        prop = str(ogp['property'])
+        content = str(ogp['content'])
+        if prop not in results:
+            results[prop] = []
+        results[prop].append(content)
+    return results
+
+
+def __parse_seo(metas):
+    ogps = list(filter(lambda x: x.has_attr('name'), metas))
+    results = {}
+    for ogp in ogps:
+        prop = str(ogp['name'])
+        content = str(ogp['content'])
+        if prop not in results:
+            results[prop] = []
+        results[prop].append(content)
+    return results
+
+def parsedom(dom):
     """
     Get Opengraph data or SEO data.
 
@@ -11,8 +34,16 @@ def parser(url):
     Return:
         [dict] OpenGraph and SEO data
     """
-    res = requests.get(url)
-    dom = BeautifulSoup(res.text)
-    metas = dom.select('head meta');
-    title = str(dom.select('head title').string);
-    
+    metas = dom.select('meta');
+    metas = list(filter(lambda x: x.has_attr('content'), metas))
+
+    title = str(dom.find('head').find('title').string);
+        
+    return {
+        'title': title,
+        'ogp': __parse_ogp(metas),
+        'seo': __parse_seo(metas)
+    }
+
+def request(url):
+    pass
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,8 +1,37 @@
+import json
+from bs4 import BeautifulSoup
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from ogp_parser.parser import parsedom
 
-from ogp_parser.parser import parser
-from setuptools import find_packages
 
-parser('http://ogp.me')
+def check_keys(dst, gt):
+    dst_keylist = sorted(dst.keys())
+    gt_keylist = sorted(gt.keys())
+    assert len(dst_keylist) == len(gt_keylist), '親のキーの数が違う'
+    for key in gt_keylist:
+        assert key in dst_keylist, f'キー"{key}"がない'
+
+def check_data(dst, gt, datakey):
+    for key, values in gt[datakey].items():
+        assert len(values) == len(dst[datakey][key]), f'件数不一致[key={key}]'
+        val_dst = sorted(values)
+        val_gt  = sorted(dst[datakey][key])
+        for idx, value in enumerate(val_gt):
+            assert value == val_dst[idx], '値不一致'
+
+thisdir = os.path.dirname(__file__)
+srcfile = os.path.join(thisdir, 'dummy.html')
+dstfile = os.path.join(thisdir, 'dummy_result.json')
+with open(srcfile, 'r') as f:
+    dom = BeautifulSoup(f.read())
+with open(dstfile, 'r') as f:
+    gt = json.loads(f.read())
+
+result = parsedom(dom)
+
+assert gt['title'] == result['title'], 'タイトルが違う!!'
+check_keys(result, gt)
+check_data(result, gt, 'ogp')
+check_data(result, gt, 'seo')
